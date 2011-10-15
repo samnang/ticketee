c10045ba203fc257f2002e7dbdde800d31814bbf
Samnang Chhun <samnang.chhun@gmail.com>
2011-10-16 00:50:36 +0700

    Users can now upload 3 files at a time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111015171518) do
+ActiveRecord::Schema.define(:version => 20111015173946) do
+
+  create_table "assets", :force => true do |t|
+    t.string   "asset_file_name"
+    t.integer  "asset_file_size"
+    t.string   "asset_content_type"
+    t.datetime "asset_updated_at"
+    t.integer  "ticket_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "permissions", :force => true do |t|
     t.integer  "user_id"
@@ -35,10 +45,6 @@ ActiveRecord::Schema.define(:version => 20111015171518) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "asset_file_name"
-    t.string   "asset_content_type"
-    t.integer  "asset_file_size"
-    t.datetime "asset_updated_at"
   end
 
   add_index "tickets", ["project_id"], :name => "index_tickets_on_project_id"
--- features/creating_tickets.feature
+++ features/creating_tickets.feature
@@ -38,7 +38,11 @@ Feature: Creating Tickets
   Scenario: Creating a ticket with an attachment
     When I fill in "Title" with "Add documentation for blink tag"
     And I fill in "Description" with "The blink tag has a speed attribute"
-    And I attach the file "spec/fixtures/speed.txt" to "File"
+    And I attach the file "spec/fixtures/speed.txt" to "File #1"
+    And I attach the file "spec/fixtures/spin.txt" to "File #2"
+    And I attach the file "spec/fixtures/gradient.txt" to "File #3"
     And I press "Create Ticket"
     Then I should see "Ticket has been created."
-    And I should see "speed.txt" within "#ticket .asset"
+    And I should see "speed.txt" within "#ticket .assets"
+    And I should see "spin.txt" within "#ticket .assets"
+    And I should see "gradient.txt" within "#ticket .assets"
