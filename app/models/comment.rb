1b6a7a78bdc0a1aeb0a53b63f9ccd6d232e08cc1
Samnang Chhun <samnang.chhun@gmail.com>
2011-10-16 23:08:05 +0700

    Added email ticket notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,12 +11,12 @@ class Comment < ActiveRecord::Base
   before_create :set_previous_state
   after_create :set_ticket_state
 
+  def set_previous_state
+    self.previous_state = ticket.state
+  end
+
   def set_ticket_state
     self.ticket.state = self.state
     self.ticket.save
-  end
-
-  def set_previous_state
-    self.previous_state = ticket.state
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,11 +6,15 @@ class Ticket < ActiveRecord::Base
   has_many :comments
   has_many :assets
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :watchers, :join_table => "ticket_watchers",
+                                     :class_name => "User"
 
   accepts_nested_attributes_for :assets
 
   validates :title, :presence => true
   validates :description, :presence => true, :length => {:minimum => 10}
+
+  after_create :creator_watches_me
 
   searcher do
     label :tag,  :from => :tags, :field => :name
@@ -23,5 +27,9 @@ class Ticket < ActiveRecord::Base
     end
 
     self.tags << tags
+  end
+
+  def creator_watches_me
+    self.watchers << self.user
   end
 end
