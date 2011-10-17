f9ba6ef951ea5922381cc87b1034c7f65618c575
Samnang Chhun <samnang.chhun@gmail.com>
2011-10-17 12:49:45 +0700

    Restricting projects API show to only users who have permission to view a project

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
+  before_filter :find_project, :only => :show
+
   def index
     respond_with(Project.for(current_user))
   end
@@ -15,5 +17,14 @@ class Api::V1::ProjectsController < Api::V1::BaseController
     else
       respond_with(project)
     end
+  end
+
+  private
+
+  def find_project
+    @project = Project.for(current_user).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    error = {:error => "The project you were looking for could not be found." }
+    respond_with(error, :status => 404)
   end
 end
